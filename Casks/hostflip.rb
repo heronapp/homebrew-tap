820e30258fbf7570af0912db6d679576c564be31
heronapp <293014080+heronapp@users.noreply.github.com>
2026-08-13 12:40:08 +0800

    hostflip 0.1.5

--- Casks/hostflip.rb
+++ Casks/hostflip.rb
@@ -1,6 +1,6 @@
 cask "hostflip" do
-  version "0.1.4"
-  sha256 "28b6157ea0c1ca3de66f46f08d0e6e9c93b4ded0bc42c1b2d38dc037889a41a4"
+  version "0.1.5"
+  sha256 "049651bf22b6e2088a58cae3e641614087745e4ae66846574d936747ca247b66"
 
   auto_updates true
 
